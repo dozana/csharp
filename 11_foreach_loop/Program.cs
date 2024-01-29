1080d06bc61b7776156f9d8c085102b33dce1bfc
Levan <levan.soroznishvili@gmail.com>
2024-01-29 22:46:31 +0400

    foreach loop

---
+++ 11_foreach_loop/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _11_foreach_loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "John Doe";
+
+            for (var i = 0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+
+            Console.WriteLine("------");
+
+            // Rewrite above loop
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+
+            Console.WriteLine("------");
+
+            var numbers = new int[] { 1, 2, 3, 4 };
+            foreach(var number in numbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}

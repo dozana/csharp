dd34e33753e30619f25dbcb2e66988a9e33cd81f
Levan <levan.soroznishvili@gmail.com>
2024-01-29 22:30:14 +0400

    tasks completed

---
+++ 09_if_else_and_switch/ImageOrientation.cs
@@ -0,0 +1,11 @@
+﻿namespace _09_if_else_and_switch
+{
+    internal partial class Program
+    {
+        public enum ImageOrientation
+        {
+            Landscape,
+            Portrait
+        }
+    }
+}

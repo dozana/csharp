b12ab2e8d205ab8241d06966e7ddc709d2730f2b
Levan <levan.soroznishvili@gmail.com>
2024-01-29 08:48:07 +0400

    reference and value types updated

--- 08_reference_and_value_types/Program.cs
+++ 08_reference_and_value_types/Program.cs
@@ -6,10 +6,16 @@ using System.Threading.Tasks;
 
 namespace _08_reference_and_value_types
 {
+    public class Person
+    {
+        public int Age;
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            /*
             var a = 10;
             var b = a;
 
@@ -20,6 +26,25 @@ namespace _08_reference_and_value_types
             var array2 = array1;
             array2[0] = 0;
             Console.WriteLine(string.Format("array1[0]: {0}, array2[0]: {1}", array1[0], array2[0]));
+            */
+
+            var number = 1;
+            Increment(number);
+            Console.WriteLine(number);
+
+            var person = new Person(){ Age = 20 };
+            MakeOld(person);
+            Console.WriteLine(person.Age);
+        }
+
+        public static void Increment(int number)
+        {
+            number += 10;
+        }
+
+        public static void MakeOld(Person person)
+        {
+            person.Age += 10;
         }
     }
 }

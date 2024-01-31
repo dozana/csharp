ed08e979d2c518155d1e720c1c579520c0ec9fb7
Levan <levan.soroznishvili@gmail.com>
2024-01-31 22:30:17 +0400

    conditional examples completed

---
+++ ex_01_conditionals/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_01_conditionals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Task 1: Write a program and ask the user to enter a number. 
+                The number should be between 1 to 10. If the user enters 
+                a valid number, display "Valid" on the console. Otherwise, 
+                display "Invalid". (This logic is used a lot in applications 
+                where values entered into input boxes need to be validated.)
+             */
+            /*
+            Console.WriteLine("Enter a number. between 1 to 10 ");
+
+            var input = Console.ReadLine();
+            var number = Convert.ToInt32(input);
+
+
+            if(number >= 1 && number <= 10)
+            {
+                Console.WriteLine("Valid");
+            }
+            else
+            {
+                Console.WriteLine("Invalid");
+            }
+            */
+
+
+
+            /*
+                Task 2: Write a program which takes two numbers from the 
+                console and displays the maximum of the two.
+            */
+
+            /*
+            Console.Write("Enter Number1: ");
+            var number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter Number2: ");
+            var number2 = Convert.ToInt32(Console.ReadLine());
+
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("Max is " + max);
+            */
+
+
+
+
+            /*
+                Task 3: Write a program and ask the user to enter the 
+                width and height of an image. Then tell if the image is landscape or portrait.
+            */
+            /*
+            Console.WriteLine("Image width: ");
+            var width = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Image height: ");
+            var height = Convert.ToInt32(Console.ReadLine());
+
+            var orientation = width > height ? ImageOrientation.Landscape : ImageOrientation.Portrait;
+            Console.WriteLine("Image orientation is " + orientation);
+            */
+
+
+
+            /*
+                Your job is to write a program for a speed camera. For simplicity, ignore the details 
+                such as camera, sensors, etc and focus purely on the logic. Write a program that asks 
+                the user to enter the speed limit. Once set, the program asks for the speed of a car. 
+
+                If the user enters a value less than the speed limit, program should display Ok on the 
+                console. 
+            
+                If the value is above the speed limit, the program should calculate the number 
+                of demerit points. For every 5km/hr above the speed limit, 1 demerit points should be 
+                incurred and displayed on the console. If the number of demerit points is above 12, 
+                the program should display License Suspended.
+             */
+
+            Console.Write("What is the speed limit? ");
+            var speedLimit = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is the speed of this car? ");
+            var carSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (carSpeed < speedLimit)
+            {
+                Console.WriteLine("Ok");
+            }
+            else
+            {
+                const int kmPerDemeritPoint = 5;
+                var demeritPoints = (carSpeed - speedLimit) / kmPerDemeritPoint;
+
+                if (demeritPoints > 12)
+                {
+                    Console.WriteLine("License suspended");
+                }
+                else
+                {
+                    Console.WriteLine("Demerit points: " + demeritPoints);
+                }
+            }
+        }
+    }
+}
---
+++ ex_02/Program.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                Note: for all these exercises, ignore input validation unless otherwise directed. 
+                Assume the user enters a value in the format that the program expects. For example, 
+                if the program expects the user to enter a number, don't worry about validating if 
+                the input is a number or not. When testing your program, simply enter a number.
+            */
+
+            /*
+                1) Write a program to count how many numbers between 1 and 100 are divisible by 3 with 
+                no remainder. Display the count on the console.
+            */
+
+            /*
+            var count = 0;
+
+            for (var i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    count++;
+                }
+            }
+
+            Console.WriteLine("There are {0} numbers divisible by 3 between 1 and 100.", count);
+            */
+
+
+
+            /*
+                2) Write a program and continuously ask the user to enter a number or "ok" to exit. 
+                Calculate the sum of all the previously entered numbers and display it on the console.
+             */
+
+
+            /*
+            int sum = 0;
+
+            while (true)
+            {
+                Console.Write("Write a number: ");
+
+                string input = Console.ReadLine();
+                
+                if(input.ToLower() == "ok")
+                {
+                    break;
+                }
+
+                int number = Convert.ToInt32(input);
+
+                sum += number;
+
+                Console.WriteLine(sum);
+            }
+            */
+
+            /*
+            var sum = 0;
+
+            while (true)
+            {
+                Console.Write("Enter a number (or 'ok' to exit): ");
+                var input = Console.ReadLine();
+
+                if (input.ToLower() == "ok")
+                    break;
+
+                sum += Convert.ToInt32(input);
+
+                Console.WriteLine("Sum of all numbers is: " + sum);
+            }
+            */
+
+
+
+
+
+
+            /*
+                3) Write a program and ask the user to enter a number. Compute the factorial of the 
+                number and print it on the console. For example, if the user enters 5, the program 
+                should calculate 5 x 4 x 3 x 2 x 1 and display it as 5! = 120.
+             */
+
+            /*
+            Console.Write("Enter a number: ");
+
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            var factorial = 1;
+
+            for (var i = 1; i <= number; i++)
+            {
+                factorial *= i;
+            }
+
+            Console.WriteLine("{0}! = {1}", number, factorial);
+            */
+
+
+            /*
+            Console.Write("Enter number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            if (n == 0)
+            {
+                Console.WriteLine(1); ;
+            }
+
+            long result = 1;
+
+            for (int i = n; i > 1; i--)
+            {
+                result *= i;
+            }
+
+            Console.WriteLine(result);
+            */
+
+
+
+            /*
+                4)
+
+                Write a program that picks a random number between 1 and 10. Give the user 4 chances 
+                to guess the number. If the user guesses the number, display “You won"; otherwise, 
+                display “You lost". (To make sure the program is behaving correctly, you can display 
+                the secret number on the console first.)
+             */
+
+            /*
+            var number = new Random().Next(1, 10);
+
+            Console.WriteLine("Secret is: " + number);
+
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Gues the secret number: ");
+
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("You lost!");
+            */
+
+
+            /*
+            var number = new Random().Next(1, 10);
+            Console.WriteLine("Secret is " + number);
+
+            for (var i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Guess the secret number: ");
+                var guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess == number)
+                {
+                    Console.WriteLine("You won!");
+                    return;
+                }
+            }
+
+            Console.WriteLine("You lost!");
+            */
+
+
+
+
+            /*
+                5) Write a program and ask the user to enter a series of numbers separated by comma. 
+                Find the maximum of the numbers and display it on the console. For example, if the 
+                user enters “5, 3, 8, 1, 4", the program should display 8.
+             */
+            Console.WriteLine("Enter comma separated numbers: ");
+
+            var input = Console.ReadLine();
+
+            var numbers = input.Split(',');
+
+            var max = Convert.ToInt32(numbers[0]);
+
+            foreach (var str in numbers)
+            {
+                var number = Convert.ToInt32(str);
+
+                if (number > max)
+                {
+                    max = number;
+                }
+            }
+
+            Console.WriteLine("Max is " + max);
+
+        }
+    }
+}

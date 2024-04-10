e1e20216bccabe00ecf76557f9ebc90cdf6c2667
Levan <levan.soroznishvili@gmail.com>
2024-04-10 23:57:08 +0400

    addition of to int numbers

---
+++ Examples/2_1_addition_of_to_integers/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_1_addition_of_to_integers
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int number1, number2, sum;
+
+            number1 = Convert.ToInt32(textBox1.Text);
+            number2 = Convert.ToInt32(textBox2.Text);
+            sum = number1 + number2;
+
+            label3.Text = sum.ToString();
+
+        }
+    }
+}

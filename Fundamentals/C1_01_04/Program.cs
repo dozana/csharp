2b6432609864eebba71b8b49208dd524b2fcd42b
Levan <levan.soroznishvili@gmail.com>
2024-04-17 23:56:53 +0400

    a * a

--- Fundamentals/C1_01_04/Program.cs
+++ Fundamentals/C1_01_04/Program.cs
@@ -1,16 +1,18 @@
-﻿//using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace C1_01_01
+namespace C1_01_04
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Welcome to coding");
+            var size = 5;
+            Console.WriteLine("Size = " + size);
+            Console.WriteLine("Area = " + size * size);
         }
     }
 }

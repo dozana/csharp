3bb917ba7819d42abd46a05b09c3b71d78c4a79a
Levan <levan.soroznishvili@gmail.com>
2024-04-18 23:58:14 +0400

    output

---
+++ Fundamentals/01-output/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("I am Learning C#");
+            Console.WriteLine("It is awesome!");
+
+            Console.Write("Hello World! ");
+            Console.Write("I will print on the same line.");
+        }
+    }
+}
